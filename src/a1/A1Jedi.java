2ba72973fe9598360d39b90332fd7a46b2f8b9dc
yingkelu <yingkelu@yingkede-mbp.wireless-1x.unc.edu>
2019-09-01 21:17:48 -0400

    A1

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,7 +9,7 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		int totalItnum = 0, totalCusnum = 0, totalBuynum = 0, buyNum = 0;
-		String cusName = "", buyNam = "";
+		String cusName = "";
 		
 		totalItnum = scan.nextInt();
 		String[] itName = new String[totalItnum];
@@ -21,27 +21,35 @@ public class A1Jedi {
 			itPrice[countIt] = scan.nextDouble();
 		}
 		totalCusnum = scan.nextInt();
+		int[] eachCusnumCopy=new int[totalItnum];
 		for(int countCus = 0; countCus < totalCusnum; countCus ++) {
+			for(int i=0;i<eachCusnum.length;i++) {
+			     eachCusnumCopy[i]=eachCusnum[i];
+			    }
 			cusName = scan.next()+scan.next();
 			totalBuynum = scan.nextInt();
+			String[] buyNam = new String[totalBuynum];
 			for (int countEvitem = 0; countEvitem < totalBuynum; countEvitem ++) {
 				buyNum = scan.nextInt();
-				buyNam=scan.next();
+				buyNam[countEvitem]=scan.next();
 				for (int countIt = 0; countIt < totalItnum; countIt ++) {
-					if (itName[countIt].equals(buyNam)) {
+					if (itName[countIt].equals(buyNam[countEvitem])) {
 						eachItnum[countIt]+=buyNum;
-						eachCusnum[countIt]+=1;
+						eachCusnum[countIt]++;
+							}
+						}
 					}
+				for(int i=0;i<eachCusnum.length;i++) {
+					if(eachCusnum[i]-eachCusnumCopy[i]>1) eachCusnum[i]=eachCusnumCopy[i]+1;
+					}
+			   	}
+			for (int countIt = 0; countIt < totalItnum; countIt ++) {
+				if (eachCusnum[countIt]== 0) {
+					System.out.println("No customers bought "+itName[countIt]);
+				} else {
+					System.out.println(eachCusnum[countIt]+" customers bought " + eachItnum[countIt] + " " +itName[countIt]);
 				}
 			}
-		}
-		for (int countIt = 0; countIt < totalItnum; countIt ++) {
-			if (eachCusnum[countIt]== 0) {
-				System.out.println("No customers bought "+itName[countIt]);
-			} else {
-				System.out.println(eachCusnum[countIt]+" customers bought " + eachItnum[countIt] + " " +itName[countIt]);
-			}
-		}
 	scan.close();	
-	}
+		}
 }
